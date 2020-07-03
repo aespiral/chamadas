857d6451b8531087757ffb3556db4809ca03c77d
Lanfig <orlandoaf@gmail.com>
2020-07-03 09:23:52 -0300

    Melhorias em errado.c

--- Encontros/03/errado.c
+++ Encontros/03/errado.c
@@ -4,7 +4,10 @@
 
 int main() {
     int fd = open("delete.me", O_RDONLY, 0);
-    printf("%d\n", errno);
-    perror("O erro foi: ");
+    printf("\nENOENT corresponde ao erro de no %d e o número obtido foi %d\n", ENOENT, errno);
+    if (errno == ENOENT)
+        printf("\nErro ENOENT (significaria 'NO ENTry' - 'nenhuma entrada'?) \n");
+    perror("\nMensagem de erro em uma apresentação amigável, dada pelo sistema: ");
+
     return 0;
 }
